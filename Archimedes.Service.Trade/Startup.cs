1d62d596131e37afe6061c574cccc3f42773019e
Simon Burton <burt76@outlook.com>
2020-11-05 12:53:08 +0000

    gg

--- Archimedes.Service.Trade/Startup.cs
+++ Archimedes.Service.Trade/Startup.cs
@@ -1,6 +1,7 @@
 using Archimedes.Library.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace Archimedes.Service.Trade
 
             services.AddLogging();
 
-            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +38,7 @@ namespace Archimedes.Service.Trade
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
