c723ef7475cc0e5c2efcd64a397a37fab3893584
Simon Burton <burt76@outlook.com>
2020-11-05 11:46:39 +0000

    added health controller

--- Archimedes.Service.Trade/Startup.cs
+++ Archimedes.Service.Trade/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Archimedes.Library.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,10 @@ namespace Archimedes.Service.Trade
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<Config>(Configuration.GetSection("AppSettings"));
+            services.AddSingleton(Configuration);
+            var config = Configuration.GetSection("AppSettings").Get<Config>();
+
             services.AddControllers();
         }
 
