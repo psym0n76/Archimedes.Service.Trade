8b2bf175169673e9ff38999c4d47a2bfd8e94662
Simon Burton <burt76@outlook.com>
2021-01-10 16:24:19 +0000

    update

--- Archimedes.Service.Trade/Loaders/CandleLoader.cs
+++ Archimedes.Service.Trade/Loaders/CandleLoader.cs
@@ -66,7 +66,7 @@ namespace Archimedes.Service.Trade
                 retry++;
             }
             
-            _logger.LogError($"ValidateRecentCandle - Out of Date Candle {market} {granularity} {lastCandle}");
+            _logger.LogWarning(_batchLog.Print(_logId, $"ValidateRecentCandle - Out of Date Candle {market} {granularity} {lastCandle}"));
         }
 
 
